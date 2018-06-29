fa61d4926c875f7d947218623d778de1c25e010e
Steven Lum <stevielum1@gmail.com>
2018-06-29 14:12:09 -0700

    Add Hashing to arrays, strings,hashes

--- my_hash_map/lib/p01_int_set.rb
+++ my_hash_map/lib/p01_int_set.rb
@@ -1,14 +1,19 @@
 class MaxIntSet
   def initialize(max)
+    @store = Array.new(max+1) { false }
   end
 
   def insert(num)
+    raise "Out of bounds" if @store.length <= num || num < 0
+    @store[num] = true
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    @store[num]
   end
 
   private
@@ -27,18 +32,23 @@ class IntSet
   end
 
   def insert(num)
+    self[num] << num unless self[num].include?(num)
   end
 
   def remove(num)
+    self[num].delete(num) if self[num].include?(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    bucket = num % num_buckets
+    @store[bucket]
   end
 
   def num_buckets
@@ -55,18 +65,32 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if count == num_buckets
+    unless self[num].include?(num)
+      self[num] << num
+      @count += 1
+    end
   end
 
   def remove(num)
+    if self[num].include?(num)
+      self[num].delete(num)
+      @count -= 1
+      return true
+    end
+    false
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    bucket = num % num_buckets
+    @store[bucket]
   end
 
   def num_buckets
@@ -74,5 +98,13 @@ class ResizingIntSet
   end
 
   def resize!
+    store_dup = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |num|
+        new_bucket = num % store_dup.length
+        store_dup[new_bucket] << num
+      end
+    end
+    @store = store_dup
   end
 end
--- my_hash_map/lib/p02_hashing.rb
+++ my_hash_map/lib/p02_hashing.rb
@@ -4,11 +4,16 @@ end
 
 class Array
   def hash
+    array_hash = self.each_with_index.map do |el, idx|
+      (el * (idx+1)).hash
+    end
+    array_hash.reduce(:+).hash
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +21,8 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.to_a.sort.map do |el|
+      (el[0].to_s.ord.hash + el[1].to_s.ord.hash).hash
+    end.hash
   end
 end
