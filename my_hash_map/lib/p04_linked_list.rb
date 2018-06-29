83a1c88e67a6c8b3ac880bccdd60ee9ef008a1c6
Steven Lum <stevielum1@gmail.com>
2018-06-29 15:12:13 -0700

    Add finished linked list class

--- my_hash_map/lib/p04_linked_list.rb
+++ my_hash_map/lib/p04_linked_list.rb
@@ -13,49 +13,85 @@ class Node
   end
 
   def remove
-    # optional but useful, connects previous link to next link
-    # and removes self from list.
+    @next.prev = self.prev
+    @prev.next = self.next
   end
 end
 
 class LinkedList
+  attr_reader :head, :tail
+
+  include Enumerable
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
+
   def [](i)
-    each_with_index { |link, j| return link if i == j }
+    each_with_index { |link, j| return link if j == i }
     nil
   end
 
   def first
+    head.next
   end
 
   def last
+    tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
+#returns value
   def get(key)
+    each do |node|
+      return node.val if node.key == key
+    end
   end
 
   def include?(key)
+    each do |node|
+      return true if node.key == key
+    end
+    false
   end
 
   def append(key, val)
+     new_node = Node.new(key, val)
+     new_node.prev = @tail.prev
+     @tail.prev.next = new_node
+     new_node.next = @tail
+     @tail.prev = new_node
   end
 
   def update(key, val)
+    each do |node|
+      node.val = val if node.key == key
+    end
   end
 
   def remove(key)
+    each do |node|
+      node.remove if node.key == key
+    end
   end
 
-  def each
+  def each(&prc)
+    prc ||= proc{|node| node }
+    curr_node = @head.next
+    until curr_node.next.nil?
+      prc.call(curr_node)
+      curr_node = curr_node.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
