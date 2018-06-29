cad61af7079eb135c0ccb5216dabb50b78ce5805
Steven Lum <stevielum1@gmail.com>
2018-06-29 14:21:02 -0700

    Add phase 3, hashset

--- my_hash_map/lib/p03_hash_set.rb
+++ my_hash_map/lib/p03_hash_set.rb
@@ -1,3 +1,5 @@
+require_relative 'p02_hashing'
+
 class HashSet
   attr_reader :count
 
@@ -7,24 +9,48 @@ class HashSet
   end
 
   def insert(key)
-  end
-
-  def include?(key)
+    resize! if count == num_buckets
+    unless self[key].include?(key)
+      self[key] << key
+      @count += 1
+    end
   end
 
   def remove(key)
+    if self[key].include?(key)
+      self[key].delete(key)
+      @count -= 1
+      return true
+    end
+    false
+  end
+
+  def include?(key)
+    self[key].include?(key)
   end
 
   private
 
-  def [](num)
+  def [](key)
     # optional but useful; return the bucket corresponding to `num`
+    bucket = key.hash % num_buckets
+    @store[bucket]
   end
+
 
   def num_buckets
     @store.length
   end
 
   def resize!
+    store_dup = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |bucket|
+      bucket.each do |key|
+        new_bucket = key.hash % store_dup.length
+        store_dup[new_bucket] << key
+      end
+    end
+    @store = store_dup
   end
+
 end
